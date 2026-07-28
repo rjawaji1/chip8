9ae917cb12c02fd51950ffee73c6d9d1f7d0a6b9
Rajeev Athreya Jawaji <contact@rjawaji.com>
2026-07-28 23:04:10 +1000

    docs: Add comments for the currosponding instructions as per cowgods documentation

--- src/cpu.c
+++ src/cpu.c
@@ -60,18 +60,30 @@ void cpu_step(Cpu *cpu) {
 }
 
 static inline void op_0(Cpu *cpu, uint16_t opcode) {
-	if (opcode == 0x00E0) {
+	switch (opcode) {
+	case 0x00E0: // CLS - Clear the display
 		memset(cpu->vram, 0, sizeof(cpu->vram));
-	} else if (opcode == 0x00EE) {
+		break;
+
+	case 0x00EE: // RET - Return from subroutine
 		cpu->pc = cpu->stack[cpu->sp] - 2;
 		cpu->sp--;
+		break;
 	}
 }
 
+/*
+ * 1nnn - JP addr
+ * Jump to location nnn
+ */
 static inline void op_1(Cpu *cpu, uint16_t opcode) {
 	cpu->pc = (opcode & 0x0FFF) - 2;
 }
 
+/*
+ * 2nnn - CALL addr
+ * Call subroutine at nnn
+ */
 static inline void op_2(Cpu *cpu, uint16_t opcode) {
 	uint16_t nnn = opcode & 0x0FFF;
 
@@ -80,6 +92,10 @@ static inline void op_2(Cpu *cpu, uint16_t opcode) {
 	cpu->pc = nnn - 2;
 }
 
+/*
+ * 3xkk - SE Vx, byte
+ * Skip next instruction if Vx = kk.
+ */
 static inline void op_3(Cpu *cpu, uint16_t opcode) {
 	uint8_t x = (opcode & 0x0F00) >> 8;
 	uint8_t kk = opcode & 0x00FF;
@@ -87,6 +103,10 @@ static inline void op_3(Cpu *cpu, uint16_t opcode) {
 		cpu->pc += 2;
 }
 
+/*
+ * 4xkk - SNE Vx, byte
+ * Skip next instruction if Vx != kk.
+ */
 static inline void op_4(Cpu *cpu, uint16_t opcode) {
 	uint8_t x = (opcode & 0x0F00) >> 8;
 	uint8_t kk = opcode & 0x00FF;
@@ -94,6 +114,10 @@ static inline void op_4(Cpu *cpu, uint16_t opcode) {
 		cpu->pc += 2;
 }
 
+/*
+ * 5xy0 - SE Vx, Vy
+ * Skip next instruction if Vx = Vy
+ */
 static inline void op_5(Cpu *cpu, uint16_t opcode) {
 	uint8_t x = (opcode & 0x0F00) >> 8;
 	uint8_t y = (opcode & 0x00F0) >> 4;
@@ -101,12 +125,20 @@ static inline void op_5(Cpu *cpu, uint16_t opcode) {
 		cpu->pc += 2;
 }
 
+/*
+ * 6xkk - LD Vx, byte
+ * Set Vx = kk
+ */
 static inline void op_6(Cpu *cpu, uint16_t opcode) {
 	uint8_t x = (opcode & 0x0F00) >> 8;
 	uint8_t kk = opcode & 0x00FF;
 	cpu->v[x] = kk;
 }
 
+/*
+ * 7xkk - ADD Vx, byte
+ * Set Vx = Vx + kk
+ */
 static inline void op_7(Cpu *cpu, uint16_t opcode) {
 	uint8_t x = (opcode & 0x0F00) >> 8;
 	uint8_t kk = opcode & 0x00FF;
@@ -119,30 +151,50 @@ static inline void op_8(Cpu *cpu, uint16_t opcode) {
 	uint8_t n = opcode & 0x00F0;
 
 	switch (n) {
+	// LD Vx, Vy - Stores the value of register Vy in register Vx
+	case 0x0:
+		cpu->v[x] = cpu->v[y];
+		break;
+
+	// OR vx, vy - Set Vx = Vx or Vy
 	case 0x1:
 		cpu->v[x] |= cpu->v[y];
 		break;
+
+	// AND vx, vy - Set Vx = Vx and Vy
 	case 0x2:
 		cpu->v[x] &= cpu->v[y];
 		break;
+
+	// XOR Vx, Vy - Set Vx = Vx XOR Vy.
 	case 0x3:
 		cpu->v[x] ^= cpu->v[y];
 		break;
+
+	// ADD Vx, Vy - Set Vx = Vx + Vy, set VF = carry.
 	case 0x4:
 		cpu->v[0xf] = ckd_add(&cpu->v[x], cpu->v[x], cpu->v[y]);
 		break;
+
+	// SUB Vx, Vy - Set Vx = Vx - Vy, set VF = NOT borrow.
 	case 0x5:
 		cpu->v[0xF] = cpu->v[x] > cpu->v[y];
 		cpu->v[x] -= cpu->v[y];
 		break;
+
+	// SHR Vx {, Vy} - Set Vx = Vx SHR 1
 	case 0x6:
 		cpu->v[0xF] = (cpu->v[x] & 0b00000001) == 1;
 		cpu->v[x] >>= 1;
 		break;
+
+	// SUBN Vx, Vy - Set Vx = Vy - Vx, set VF = NOT borrow.
 	case 0x7:
 		cpu->v[0xF] = cpu->v[y] > cpu->v[x];
 		cpu->v[x] = cpu->v[y] - cpu->v[x];
 		break;
+
+	// SHL Vx {, Vy} - Set Vx = Vx SHL 1.
 	case 0xE:
 		cpu->v[0xF] = (cpu->v[x] & 0b10000000) == 0b10000000;
 		cpu->v[x] <<= 1;
@@ -150,6 +202,10 @@ static inline void op_8(Cpu *cpu, uint16_t opcode) {
 	}
 }
 
+/*
+ * 9xy0 - SNE Vx, Vy
+ * Skip next instruction if Vx != Vy.
+ */
 static inline void op_9(Cpu *cpu, uint16_t opcode) {
 	uint8_t x = (opcode & 0x0F00) >> 8;
 	uint8_t y = (opcode & 0x00F0) >> 4;
@@ -157,20 +213,36 @@ static inline void op_9(Cpu *cpu, uint16_t opcode) {
 		cpu->pc += 2;
 }
 
+/*
+ * Annn - LD I, addr
+ * Set I = nnn.
+ */
 static inline void op_a(Cpu *cpu, uint16_t opcode) {
 	cpu->i = opcode & 0x0FFF;
 }
 
+/**
+ * Bnnn - JP V0, addr
+ * Jump to location nnn + V0.
+ */
 static inline void op_b(Cpu *cpu, uint16_t opcode) {
 	cpu->pc = cpu->i + cpu->v[0] - 2;
 }
 
+/**
+ * Cxkk - RND Vx, byte
+ * Set Vx = random byte AND kk.
+ */
 static inline void op_c(Cpu *cpu, uint16_t opcode) {
 	uint8_t x = (opcode & 0x0F00) >> 8;
 	uint8_t kk = opcode & 0x00FF;
 	cpu->v[x] = (uint8_t)rand() & kk;
 }
 
+/**
+ * Dxyn - DRW Vx, Vy, nibble
+ * Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+ */
 static inline void op_d(Cpu *cpu, uint16_t opcode) {
 	uint8_t x_pos = cpu->v[(opcode & 0x0F00) >> 8];
 	uint8_t y_pos = cpu->v[(opcode & 0x00F0) >> 4];
@@ -181,8 +253,9 @@ static inline void op_d(Cpu *cpu, uint16_t opcode) {
 		sprite = cpu->memory[cpu->i + i];
 
 		byte = x_pos >> 3; // Divide by 8
-		bit = x_pos & 7;   // Get the remaider of 8 which is the last 3 bits
+		bit = x_pos & 7;   // Get the modulo of 8 which is the last 3 bits
 
+		// Calculate the overflow for when it wraps around
 		left = sprite << (8 - bit);
 		right = sprite >> bit;
 
